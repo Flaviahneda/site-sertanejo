21a00e911bc0a24456b5fecc5f675857fa88340f
Flaviahneda <145030821+Flaviahneda@users.noreply.github.com>
2025-04-03 09:47:01 -0300

    ddd

--- script.js
+++ script.js
@@ -36,6 +36,6 @@ botaoDeAcessibilidade.addEventListener('click', function (){
    alternaContraste.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste')
 })
-const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
-const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+const botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+const botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
 botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
